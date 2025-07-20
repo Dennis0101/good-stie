1f5c107e6030b1c0618d9f435eed8fafbb093a67
Dennis0101 <67768185+Dennis0101@users.noreply.github.com>
2025-07-20 14:44:40 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-neon': '#e81cff',
+      },
+    },
+  },
+  plugins: [],
+};
